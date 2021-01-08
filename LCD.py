fed03e3feed20fa77cb4262b19c01e5d199f96a1
JasperWithofs <withofs.jasper@gmail.com>
2021-01-08 10:26:10 +0100

    Code LCD 1.0.2 Commentaar toegevoegd om code duidelijker te maken

--- LCD.py
+++ LCD.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+#Een programma dat op een LCD je de datum tijd en ip address van u rpi ziet
             
 # Imports
 
@@ -13,15 +14,15 @@ import adafruit_character_lcd.character_lcd as characterlcd
 
 __author__ = "Jasper Withofs"
 __email__ = "jasper.withofs@student.kdg.be"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __status__ = "Development"
 
 
-
+#pas dit aan naargelang de grote van de lcd
 lcd_columns = 16
 lcd_rows = 2
 
-
+#IO / LCD Initialiseren
 lcd_rs = digitalio.DigitalInOut(board.D22)
 lcd_en = digitalio.DigitalInOut(board.D17)
 lcd_d4 = digitalio.DigitalInOut(board.D25)
@@ -32,7 +33,7 @@ lcd_d7 = digitalio.DigitalInOut(board.D18)
 lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
                                       lcd_d7, lcd_columns, lcd_rows)
 
-
+#ethernet/wifi device zoeken
 def find_interface():
     find_device = "ip addr show"
     interface_parse = run_cmd(find_device)
@@ -41,7 +42,7 @@ def find_interface():
             dev_name = line.split(':')[1]
     return dev_name
 
-
+#IP zoeken
 def parse_ip():
     find_ip = "ip addr show %s" % interface
     find_ip = "ip addr show %s" % interface
@@ -52,7 +53,7 @@ def parse_ip():
             ip = ip.split('/')[0]
     return ip
 
-
+#shell command dat ASCII teruggeeft
 def run_cmd(cmd):
     p = Popen(cmd, shell=True, stdout=PIPE)
     output = p.communicate()[0]
@@ -61,15 +62,17 @@ def run_cmd(cmd):
 
 lcd.clear()
 
+#detecteren van actieve network devices
 sleep(2)
 interface = find_interface()
 ip_address = parse_ip()
 
 while True:
+    #Datum & tijd
     lcd_line_1 = datetime.now().strftime('%b %d  %H:%M:%S\n')
-
+    #IP Adress
     lcd_line_2 = "IP " + ip_address
-
+    #2 lijnen samenvoegen
     lcd.message = lcd_line_1 + lcd_line_2
 
     sleep(2)
